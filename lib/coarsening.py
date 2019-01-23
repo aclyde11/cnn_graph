f3e7e3726a22327ad77046e1223d3223917dccf2
austin <aclyde11@gmail.com>
2019-01-23 15:46:23 -0600

    t

--- lib/coarsening.py
+++ lib/coarsening.py
@@ -226,6 +226,7 @@ def perm_data(x, indices):
 
     N, M = x.shape
     Mnew = len(indices)
+    print (len(indices))
     assert Mnew >= M
     xnew = np.empty((N, Mnew))
     for i,j in enumerate(indices):
